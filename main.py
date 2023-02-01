1e39a48bf096135f7b85f60b0f65362088dd0ed1
Devin Custodio <devincustodio@Devins-MacBook-Air.local>
2023-01-31 20:32:10 -0500

    added ffmpeg code

--- main.py
+++ main.py
@@ -1,54 +1,65 @@
-from pydub import AudioSegment
-import whisper
-from moviepy.editor import VideoFileClip, TextClip
-from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+# from pydub import AudioSegment
+# import whisper
+# from moviepy.editor import VideoFileClip, TextClip
+# from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 
-def convert_to_srt(result):
-    # Initialize the counter for the srt file's subtitle number
-    subtitle_num = 1
+# def convert_to_srt(result):
+#     # Initialize the counter for the srt file's subtitle number
+#     subtitle_num = 1
 
-    # Open a file for writing
-    with open("clip.srt", "w") as file:
-        # Iterate over each segment in the result
-        for segment in result['segments']:
-            # Write the subtitle number
-            file.write(str(subtitle_num) + "\n")
+#     # Open a file for writing
+#     with open("clip.srt", "w") as file:
+#         # Iterate over each segment in the result
+#         for segment in result['segments']:
+#             # Write the subtitle number
+#             file.write(str(subtitle_num) + "\n")
             
-            # Write the start and end times for the segment
-            start_time = str(int(segment['start'] // 60)).zfill(2) + ":" + str(int(segment['start'] % 60)).zfill(2) + "," + str(int((segment['start'] % 1) * 1000)).zfill(3)
-            end_time = str(int(segment['end'] // 60)).zfill(2) + ":" + str(int(segment['end'] % 60)).zfill(2) + "," + str(int((segment['end'] % 1) * 1000)).zfill(3)
-            file.write(start_time + " --> " + end_time + "\n")
+#             # Write the start and end times for the segment
+#             start_time = str(int(segment['start'] // 60)).zfill(2) + ":" + str(int(segment['start'] % 60)).zfill(2) + "," + str(int((segment['start'] % 1) * 1000)).zfill(3)
+#             end_time = str(int(segment['end'] // 60)).zfill(2) + ":" + str(int(segment['end'] % 60)).zfill(2) + "," + str(int((segment['end'] % 1) * 1000)).zfill(3)
+#             file.write(start_time + " --> " + end_time + "\n")
             
-            # Write the text for the segment
-            file.write(segment['text'] + "\n\n")
+#             # Write the text for the segment
+#             file.write(segment['text'] + "\n\n")
             
-            # Increment the subtitle number
-            subtitle_num += 1
+#             # Increment the subtitle number
+#             subtitle_num += 1
 
 
-# Load the mp4 file
-sound = AudioSegment.from_file("clip.mp4", format="mp4")
+# # Load the mp4 file
+# sound = AudioSegment.from_file("clip.mp4", format="mp4")
 
-# Save the mp3 file
-sound.export("clip.mp3", format="mp3")
+# # Save the mp3 file
+# sound.export("clip.mp3", format="mp3")
 
-model = whisper.load_model("base")
-result = model.transcribe('clip.mp3')
-convert_to_srt(result)
+# model = whisper.load_model("base")
+# result = model.transcribe('clip.mp3')
+# convert_to_srt(result)
 
 
-# Load the video file
-video = VideoFileClip("clip.mp4")
+# # Load the video file
+# video = VideoFileClip("clip.mp4")
 
-# Load the subtitle file
-subtitles = open("clip.srt").read()
+# # Load the subtitle file
+# subtitles = open("clip.srt").read()
 
-# Create the subtitle clip
-subtitle_clip = TextClip(subtitles, fontsize=30, color='white')
+# # Create the subtitle clip
+# subtitle_clip = TextClip(subtitles, fontsize=30, color='white')
 
-# Combine the video and subtitle clips
-final_clip = CompositeVideoClip([video, subtitle_clip.set_pos(("center", 550))])
+# # Combine the video and subtitle clips
+# final_clip = CompositeVideoClip([video, subtitle_clip.set_pos(("center", 550))])
 
-# # Save the final clip
-final_clip.duration = video.duration
-final_clip.write_videofile("final_video.mp4")
+# # # Save the final clip
+# final_clip.duration = video.duration
+# final_clip.write_videofile("final_video.mp4")
+
+import ffmpeg
+
+
+video = ffmpeg.input('test.mp4')
+audio = video.audio
+subtitles = ffmpeg.input('clip.srt').filter("subtitles", 'clip.srt')
+
+ffmpeg.concat(video.filter("scale", size="1280x720").filter("concat", n=2, v=1, a=0), subtitles, audio, v=0, a=1).output('out.mp4').run()
+
+
